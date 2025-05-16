1130f0739a8031f9a504ac88bdb3e58e83fcea6e
xviethoangdz <144246997+xviethoangdz@users.noreply.github.com>
2025-05-17 01:25:51 +0700

    xong cai cap nhap kho khi mua hang

--- backend/src/modules/iventory/iventory.module.ts
+++ backend/src/modules/iventory/iventory.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Ingredient, IngredientSchema } from '../ingredients/schemas/ingredient.schema';
 import { Recipe, RecipeSchema } from '../recipe/schemas/recipe.schema';
 import { InventoryController } from './iventory.controller';
 import { InventoryService } from './iventory.service';
 import { Inventory, InventorySchema } from './schemas/iventory.schema';
-
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Inventory.name, schema: InventorySchema },
       { name: Recipe.name, schema: RecipeSchema },
+      { name: Ingredient.name, schema: IngredientSchema },
     ]),
   ],
   controllers: [InventoryController],
